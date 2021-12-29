2d2dfa78e7a766faa57934f6e95696315f594d54
martinkabe <martinkabe@seznam.cz>
2021-12-29 16:21:55 +0100

    Improve exception handling

--- app.py
+++ app.py
@@ -28,27 +28,31 @@ def index():
 @app.route("/vysledky", methods=['GET', 'POST'])
 def vysledky():
     msg = ""
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
+    try:
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
 
-        if uploaded_file.filename.rsplit('.', 1)[1].lower() != 'csv':
-            msg = "Povoleny typ souboru je jen csv!"
-            return render_template('index.html', msg=msg)
+            if uploaded_file.filename.rsplit('.', 1)[1].lower() != 'csv':
+                msg = "Povoleny typ souboru je jen csv!"
+                return render_template('index.html', msg=msg)
 
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        uploaded_file.save(file_path)
-        data = nd.data_do_matice(file_path)
-        os.remove(file_path)
-        koeficienty_nazvy, koeficienty, sd_koeficienty, testova_kriteria, phodnoty, rsquares = ra.vypocti_odhady_koeficientu(data)
-        session['coeffs'] = {"Prediktory": koeficienty_nazvy, "Koeficienty": koeficienty}
-        regrese = {
-            "koeficienty_nazvy": koeficienty_nazvy,
-            "koeficienty": koeficienty,
-            "sd_koeficienty": sd_koeficienty,
-            "testova_kriteria": testova_kriteria,
-            "phodnoty": phodnoty
-        }
-        return render_template('results.html', regrese=regrese, rsquares=rsquares)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+            uploaded_file.save(file_path)
+            data = nd.data_do_matice(file_path)
+            os.remove(file_path)
+            koeficienty_nazvy, koeficienty, sd_koeficienty, testova_kriteria, phodnoty, rsquares = ra.vypocti_odhady_koeficientu(data)
+            session['coeffs'] = {"Prediktory": koeficienty_nazvy, "Koeficienty": koeficienty}
+            regrese = {
+                "koeficienty_nazvy": koeficienty_nazvy,
+                "koeficienty": koeficienty,
+                "sd_koeficienty": sd_koeficienty,
+                "testova_kriteria": testova_kriteria,
+                "phodnoty": phodnoty
+            }
+            return render_template('results.html', regrese=regrese, rsquares=rsquares)
+    except Exception as e:
+        msg = str(e)
+        return render_template('index.html', msg=msg)
     else:
         msg = "Nahrej soubor!"
         return render_template('index.html', msg=msg)
